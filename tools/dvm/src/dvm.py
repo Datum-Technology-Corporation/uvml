c257313b3dbb17ac2890aa6b2abd5baa89d3c0a0
David Poulin <dpoulin@datumtc.ca>
2021-10-09 01:32:06 -0400

    added html test report

--- tools/dvm/src/dvm.py
+++ tools/dvm/src/dvm.py
@@ -15,11 +15,11 @@
 """Design Verification \'Makefile\'.
 
 Usage:
-  dvm all  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]  [-q | --noclean]  [-c | --cov]
+  dvm all  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]  [-q | --noclean]  [-c | --cov] [-- <args>]
   dvm cmp  <target>
   dvm elab <target>  [-d | --debug]
   dvm cpel <target>
-  dvm sim  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]  [-c | --cov]
+  dvm sim  <target>  [-t <test_name>]  [-s <seed>]  [-g | --gui]  [-w | --waves]  [-c | --cov] [-- <args>]
   dvm clean
   dvm results  <target> <filename>
   dvm cov      <target>
@@ -52,6 +52,8 @@ from datetime import datetime
 from yaml.loader import SafeLoader
 import xml.etree.cElementTree as ET
 import re
+import jinja2
+from jinja2 import Template
 
 
 dbg             = False
@@ -73,6 +75,59 @@ history_file_path = pwd + "/history.yaml"
 uvm_warning_regex = "UVM_WARNING\s+(.+)\((\d+)\)\s+\@\s+(\d+\.\d+\s+ns)\:\s+(\w+)\s+\[(\w+)\](.+)"
 uvm_error_regex   = "UVM_ERROR\s+(.+)\((\d+)\)\s+\@\s+(\d+\.\d+\s+ns)\:\s+(\w+)\s+\[(\w+)\](.+)"
 uvm_fatal_regex   = "UVM_FATAL\s+(.+)\((\d+)\)\s+\@\s+(\d+\.\d+\s+ns)\:\s+(\w+)\s+\[(\w+)\](.+)"
+
+html_report_template = Template("""
+<!doctype html>
+<html lang="en">
+<head title="{{ testsuites.name }}">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+</head>
+<body>
+{% if testsuites.passed %}
+<h1>Simulation Test Results for {{ testsuites.name }} - <span style="font-weight: bold;">{{ testsuites.failures }} Failures</span> ({{ testsuites.timestamp }})</h1>
+{% else %}
+<h1>Simulation Test Results for {{ testsuites.name }} - <span style="font-weight: bold; color: red;">{{ testsuites.failures }} Failures</span> ({{ testsuites.timestamp }})</h1>
+{% endif %}
+{% for suite in testsuites.suites %}
+<div style="padding: 10px; background-color: #EEEEEE;">
+<h2>{{ suite.name }} - {{ suite.num_tests }} tests</h2>
+<h3><span style="color: red;">{{ suite.failures }} failing</span> - <span style="color: green;">{{ suite.passing }} passing</span></h3>
+<table class="table table-hover table-condensed" style="background-color: white;">
+<thead>
+<tr>
+<th>#</th>
+<th>Name</th>
+<th>Seed</th>
+<th>#Warnings</th>
+<th>#Errors</th>
+<th>Duration (sec)</th>
+<th>Result</th>
+</tr>
+</thead>
+<tbody>
+{% for test in suite.tests %}
+{% if test.passed %}
+<tr>
+{% else %}
+<tr class="danger">
+{% endif %}
+<th>{{ test.index }}</th>
+<td>{{ test.name }}</td>
+<td>{{ test.seed }}</td>
+<td>{{ test.num_warnings }}</td>
+<td>{{ test.num_errors }}</td>
+<td>{{ test.time }}</td>
+<td>{{ test.conclusion }}</td>
+</tr>
+{% endfor %}
+</tbody>
+</table>
+</div>
+{% endfor %}
+</body>
+</html>
+""")
 
 
 def do_dispatch(args):
@@ -138,7 +193,7 @@ def do_dispatch(args):
         #do_elab_rtl(args['<target>'])
         do_elab(args['<target>'], args['<target>'] + "_tb")
     if args['sim']:
-        do_sim(args['<target>'], args['<test_name>'], args['<seed>'], [])
+        do_sim(args['<target>'], args['<test_name>'], args['<seed>'], args['<args>'].split())
     if args['results']:
         do_parse_results(args['<target>'], args['<filename>'])
     if args['cov']:
@@ -220,7 +275,7 @@ def do_sim(lib_name, name, seed, plus_args):
     
     tests_results_path = pwd + "/results/" + test_name + "_" + str(seed)
     
-    orig_plus_args = plus_args
+    orig_plus_args = list(plus_args)
     plus_args.append("SIM_DIR_RESULTS="                 + pwd + "/results")
     plus_args.append("UVM_TESTNAME="                    + test_name + "_c")
     plus_args.append("UVML_FILE_BASE_DIR_SIM="          + pwd)
@@ -353,6 +408,8 @@ def add_sim_to_history_log(snapshot, test_name, seed, args, results_path):
             cur_yaml[snapshot]['simulations'][results_path]['args'] = args
             cur_yaml[snapshot]['simulations'][results_path]['results'] = results_path
             cur_yaml[snapshot]['simulations'][results_path]['start'] = timestamp
+            cur_yaml[snapshot]['simulations'][results_path]['cov'] = sim_cov
+            cur_yaml[snapshot]['simulations'][results_path]['waves'] = sim_waves
             with open(history_file_path,'w') as yamlfile:
                 yaml.dump(cur_yaml, yamlfile) # Also note the safe_dump
 
@@ -385,14 +442,28 @@ def do_parse_results(snapshot, filename):
     test_count = 0
     failure_count = 0
     total_duration = 0
+    
     now = datetime.now()
     timestamp = now.strftime("%Y/%m/%d-%H:%M:%S")
+    
     testsuites = ET.Element("testsuites")
     testsuites.set('id', timestamp)
     testsuites.set('name', snapshot)
     testsuite = ET.SubElement(testsuites, "testsuite")
     testsuite.set('id', timestamp)
     testsuite.set('name', "functional")
+    
+    suite_model = {}
+    suite_model['id'] = timestamp
+    suite_model['name'] = 'Functional'
+    suite_model['tests'] = []
+    results_model = {}
+    results_model['testsuites'] = {}
+    results_model['testsuites']['suites'] = []
+    results_model['testsuites']['name'] = snapshot
+    results_model['testsuites']['timestamp'] = timestamp
+    results_model['testsuites']['suites'].append(suite_model)
+    
     print("Parsing results ...")
     if not os.path.exists(history_file_path):
         sys.exit("No history log file")
@@ -404,20 +475,33 @@ def do_parse_results(snapshot, filename):
                     sim_log_path = sim + "/sim.log"
                     duration = int(cur_yaml[snapshot]['simulations'][sim]['duration'])
                     total_duration = total_duration + duration
+                    
                     testcase = ET.SubElement(testsuite, "testcase")
                     testcase.set('id', snapshot + "." + cur_yaml[snapshot]['simulations'][sim]['test_name'])
                     testcase.set('name', cur_yaml[snapshot]['simulations'][sim]['test_name'])
-                    #testcase.set('args', cur_yaml[snapshot]['simulations'][sim]['args'])
+                    testcase.set('time', str(duration))
+                    testcase.set('seed', str(cur_yaml[snapshot]['simulations'][sim]['seed']))
+                    
+                    testcase_model = {}
+                    suite_model['tests'].append(testcase_model)
+                    testcase_model['name'] = cur_yaml[snapshot]['simulations'][sim]['test_name']
+                    testcase_model['seed'] = cur_yaml[snapshot]['simulations'][sim]['seed']
+                    testcase_model['time'] = duration
+                    testcase_model['index'] = test_count
+                    
                     args = ET.SubElement(testcase, "args")
+                    testcase_model['args'] = []
                     for arg in cur_yaml[snapshot]['simulations'][sim]['args']:
                         arg_e = ET.SubElement(args, "arg")
                         arg_e.text = arg
-                    testcase.set('seed', str(cur_yaml[snapshot]['simulations'][sim]['seed']))
-                    testcase.set('time', str(duration))
-                    passed = sim_parse_sim_results(sim_log_path, testcase)
+                        testcase_model['args'].append(arg)
+                    
+                    passed = sim_parse_sim_results(sim_log_path, testcase, testcase_model)
                     if passed == "failed" or passed == "inconclusive":
                         failure_count = failure_count + 1
-                        
+                        testcase_model['passed'] = False
+                    else:
+                        testcase_model['passed'] = True
                     test_count = test_count + 1
                     
         testsuites.set('tests', str(test_count))
@@ -425,12 +509,26 @@ def do_parse_results(snapshot, filename):
         testsuites.set('time', str(total_duration))
         testsuite.set('tests', str(test_count))
         testsuite.set('failures', str(failure_count))
-        testsuites.set('time', str(total_duration))
+        testsuite.set('time', str(total_duration))
         tree = ET.ElementTree(testsuites)
-        tree.write(filename)
+        tree.write(filename + ".xml")
+        
+        suite_model['num_tests'] = test_count
+        suite_model['failures'] = failure_count
+        suite_model['passing'] = test_count - failure_count
+        suite_model['time'] = total_duration
+        results_model['testsuites']['failures'] = failure_count
+        if failure_count > 0:
+            results_model['testsuites']['passed'] = False
+        else:
+            results_model['testsuites']['passed'] = True
+        html_report_contents = html_report_template.render(testsuites=results_model['testsuites'])
+        with open(filename + ".html",'w') as htmlfile:
+            htmlfile.write(html_report_contents)
+        htmlfile.close()
 
 
-def sim_parse_sim_results(sim_log_path, testcase):
+def sim_parse_sim_results(sim_log_path, testcase, testcase_model):
     test_result = "passed"
     num_warnings=0
     num_errors = 0
@@ -461,6 +559,10 @@ def sim_parse_sim_results(sim_log_path, testcase):
     testcase.set("warnings", str(num_warnings))
     testcase.set("errors", str(num_errors))
     testcase.set("fatals", str(num_fatals))
+    testcase_model['num_warnings'] = num_warnings
+    testcase_model['num_errors'] = num_errors
+    testcase_model['num_fatals'] = num_fatals
+    testcase_model['conclusion'] = test_result
     return test_result
 
 
@@ -478,9 +580,9 @@ def gen_cov_report(sim_lib):
             if cur_yaml:
                 for sim in cur_yaml[sim_lib]['simulations']:
                     cov_path = sim + "/cov/xsim.covdb"
-                    dir_string = dir_string + " -dir " + cov_path
+                    if cur_yaml[sim_lib]['simulations'][sim]['cov']:
+                        dir_string = dir_string + " -dir " + cov_path
     
-    #run_xsim_bin("xcrg", dir_string + " -merge_dir " + pwd + "/cov_merge/" + sim_lib + " -merge_db_name " + sim_lib)
     if not os.path.exists(pwd + "/cov_report"):
         os.mkdir(pwd + "/cov_report")
     run_xsim_bin("xcrg", dir_string + " -report_format all -report_dir " + pwd + "/cov_report/" + sim_lib)
