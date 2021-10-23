dc84b23f4773ed53bdb28a847e4f0b014675fe9b
David Poulin <dpoulin@datumtc.ca>
2021-10-23 03:49:11 -0400

    Update sanity_regression.sh

--- dv/uvmt_st/bin/sanity_regression.sh
+++ dv/uvmt_st/bin/sanity_regression.sh
@@ -13,8 +13,8 @@
 
 # Launched from uvml project sim dir
 ./setup_project.py
-source ./setup_terminal.sh
-dvm cpel uvmt_st
-dvm sim uvmt_st -t traffic -s 1 -c
-dvm results uvmt_st results
-#dvm cov uvmt_st
+#source ./setup_terminal.sh
+../tools/dvm/src/dvm.py cpel uvmt_st
+../tools/dvm/src/dvm.py sim uvmt_st -t traffic -s 1 -c
+../tools/dvm/src/dvm.py results uvmt_st results
+#../tools/dvm/src/dvm.py cov uvmt_st
