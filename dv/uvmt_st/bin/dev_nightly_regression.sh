6d0dd23773fe26914599110fecb65199c11e0fe0
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-26 16:09:23 -0400

    Removing code for old MIO CLI

--- dv/uvmt_st/bin/dev_nightly_regression.sh
+++ dv/uvmt_st/bin/dev_nightly_regression.sh
@@ -6,12 +6,10 @@
 
 
 # Launched from uvml project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_st
+mio sim     uvmt_st -CE
 mio sim     uvmt_st -t traffic     -c
 mio sim     uvmt_st -t file_read   -c
 mio sim     uvmt_st -t file_write  -c
-mio sim     uvmt_st -t vector_file -c
+#mio sim     uvmt_st -t vector_file -c
 mio results uvmt_st results
 mio cov     uvmt_st
--- dv/uvmt_st/bin/sanity_regression.sh
+++ dv/uvmt_st/bin/sanity_regression.sh
@@ -6,12 +6,10 @@
 
 
 # Launched from uvml project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_st
-mio sim     uvmt_st -t traffic     -s 1 -c
-mio sim     uvmt_st -t file_read   -s 1 -c
-mio sim     uvmt_st -t file_write  -s 1 -c
-#mio sim     uvmt_st -t vector_file -s 1 -c
+mio sim     uvmt_st -CE
+mio sim     uvmt_st -S -t traffic     -s 1 -c
+mio sim     uvmt_st -S -t file_read   -s 1 -c
+mio sim     uvmt_st -S -t file_write  -s 1 -c
+#mio sim     uvmt_st -S -t vector_file -s 1 -c
 mio results uvmt_st results
 mio cov     uvmt_st
